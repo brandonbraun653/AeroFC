e1517668a67e3f20a370b442451dfbffb10cefa7
Brandon Braun <brandonbraun653@gmail.com>
2019-01-27 10:49:05 -0600

    Added project and some small bits of planning

---
+++ sw/vnv/dev/VisualStudio/AeroFC_MemoryDrivers/AeroFC_MemoryDrivers/main.cpp
@@ -0,0 +1,22 @@
+/********************************************************************************
+*   File Name:
+*       main.cpp
+*       
+*   Description:
+*       Sets up the environment for testing the primary memory drivers used on
+*       the AeroFC platform. Currently this is an SD Card and 
+*   
+*   2019 | Brandon Braun | brandonbraun653@gmail.com
+********************************************************************************/
+
+/* Chimera Includes */
+#include <Chimera/chimera.hpp>
+
+int main(void)
+{
+    ChimeraInit();
+	
+	for (;;)
+	{
+	}
+}
