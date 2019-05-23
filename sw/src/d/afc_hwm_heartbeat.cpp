3e12c93ff71904d8c53c51e80280b3718072e391
Brandon Braun <brandonbraun653@gmail.com>
2019-05-23 09:02:09 -0500

    Updated AeroFC project. Added submodules.

--- sw/src/d/afc_hwm_heartbeat.cpp
+++ sw/src/d/afc_hwm_heartbeat.cpp
@@ -1,12 +1,12 @@
 /********************************************************************************
-*   File Name:
-*       afc_hwm_heartbeat.cpp
-*
-*   Description:
-*       Implements the heartbeat driver
-*
-*   2019 | Brandon Braun | brandonbraun653@gmail.com
-********************************************************************************/
+ *   File Name:
+ *       afc_hwm_heartbeat.cpp
+ *
+ *   Description:
+ *       Implements the heartbeat driver
+ *
+ *   2019 | Brandon Braun | brandonbraun653@gmail.com
+ ********************************************************************************/
 
 /* Chimera Includes */
 #include <Chimera/gpio.hpp>
@@ -20,28 +20,28 @@ using namespace Chimera::GPIO;
 using namespace Chimera::Threading;
 
 /**
-*   A FreeRTOS thread that will pulse the heartbeat led so the user knows
-*   the system is up and running. Plus, who doesn't love LEDs?
-*
-*   @param[in]  argument    Not used
-*   @return void
-*/
-void afc_hwm_heartbeatThread(void *argument)
+ *   A FreeRTOS thread that will pulse the heartbeat led so the user knows
+ *   the system is up and running. Plus, who doesn't love LEDs?
+ *
+ *   @param[in]  argument    Not used
+ *   @return void
+ */
+void afc_hwm_heartbeatThread( void *argument )
 {
-    // TODO: This pin mapping is temporary for the SMT32F767ZI Nucleo dev board
-    GPIOClass redLed;
-    redLed.init(Port::PORTB, 14);
-    redLed.setMode(Drive::OUTPUT_PUSH_PULL, false);
-    redLed.setState(State::LOW);
+  // TODO: This pin mapping is temporary for the SMT32F767ZI Nucleo dev board
+  GPIOClass redLed;
+  redLed.init( Port::PORTB, 14 );
+  redLed.setMode( Drive::OUTPUT_PUSH_PULL, false );
+  redLed.setState( State::LOW );
 
-    signalThreadSetupComplete();
+  signalSetupComplete();
 
-    for (;;)
-    {
-        redLed.setState(State::HIGH);
-        delayMilliseconds(hwm_heartbeat_period_on_mS);
+  for ( ;; )
+  {
+    redLed.setState( State::HIGH );
+    delayMilliseconds( hwm_heartbeat_period_on_mS );
 
-        redLed.setState(State::LOW);
-        delayMilliseconds(hwm_heartbeat_period_off_mS);
-    }
+    redLed.setState( State::LOW );
+    delayMilliseconds( hwm_heartbeat_period_off_mS );
+  }
 }
--- sw/src/main.cpp
+++ sw/src/main.cpp
@@ -10,10 +10,10 @@
 ********************************************************************************/
 
 /* Chimera Includes */
+#include <Chimera/chimera.hpp>
 #include <Chimera/threading.hpp>
 
 /* Project Includes */
-//#include "aerofc_flash.hpp"
 #include <a/afc_watchdog.hpp>
 #include <d/afc_hwm_heartbeat.hpp>
 
@@ -23,7 +23,6 @@ int main(void)
 {
     ChimeraInit();
 
-    //addThread(simpleREDFS_WriteRead, "wr_rd", 500, NULL, 2, NULL);
     addThread(afc_hwm_independentWatchdogThread, "wd", 300, NULL, 2, NULL);
     addThread(afc_hwm_heartbeatThread, "hb", 100, NULL, 2, NULL);
 
