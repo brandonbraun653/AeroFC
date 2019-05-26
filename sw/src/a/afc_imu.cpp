f9833be72c478d56d9a5c4980d73a43101226041
Brandon Braun <brandonbraun653@gmail.com>
2019-05-26 16:31:38 -0500

    Sync. Added new submodules.

---
+++ sw/src/a/afc_imu.cpp
@@ -0,0 +1,16 @@
+/********************************************************************************
+ *  File Name:
+ *    afc_imu.cpp
+ *
+ *  Description:
+ *    
+ *
+ *  2019 | Brandon Braun | brandonbraun653@gmail.com
+ ********************************************************************************/
+
+/* C++ Includes */
+
+/* Chimera Includes */
+
+/* Driver Includes */
+#include <a/afc_imu.hpp>
---
+++ sw/src/a/afc_imu.hpp
@@ -0,0 +1,18 @@
+/********************************************************************************
+ *  File Name:
+ *    afc_imu.hpp
+ *
+ *  Description:
+ *    
+ *
+ *  2019 | Brandon Braun | brandonbraun653@gmail.com
+ ********************************************************************************/
+
+#pragma once 
+#ifndef AFC_IMU_HPP
+#define AFC_IMU_HPP
+
+/* Driver Includes */
+#include <lsm9ds0/lsm9ds0.hpp>
+
+#endif /* !AFC_IMU_HPP */
